c5d6e1869fca965a0248776b1ea21b910f1a99ff
Chrisyeet01 Scripts <chrisyeet01@gmail.com>
2026-01-18 12:39:41 -0500

    Updated to all executors

--- ESP.lua
+++ ESP.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Chrisyeet01Scripts/RobloxUniversalESP/refs/heads/main/Obfuscated%20Version"))()
+local function httpGet(url)
+    local req =
+        (syn and syn.request)
+        or (http and http.request)
+        or http_request
+        or request
+        or (fluxus and fluxus.request)
+
+    if req then
+        local res = req({
+            Url = url,
+            Method = "GET"
+        })
+        return res.Body
+    end
+
+    return game:HttpGet(url)
+end
+
+local source = httpGet("https://raw.githubusercontent.com/Chrisyeet01Scripts/RobloxUniversalESP/refs/heads/main/Obfuscated%20Version")
+loadstring(source)()
